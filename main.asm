81f06883af0b8288f55359c91a3ba3f41cd85731
Oscar <111403819+0XKR@users.noreply.github.com>
2026-01-27 18:12:53 -0400

    Add files via upload

--- main.asm
+++ main.asm
@@ -8,20 +8,41 @@
 .ORG 0
 .CSEG
 
-LDI R16, 0
-LDI R17, 255
-LDI R18, 4
-LDI R19, 3
+MAIN:
 
-OUT DDRD, R16 ; puerto D entrada
-OUT PORTD, R18 ; activar pullup en PD2 
-OUT DDRB, R17 ; puerto B salida
-OUT PORTB, R16 ; valor inicial cero
+	LDI R16, 0
+	LDI R17, 255
+	LDI R18, 12
+	LDI R19, 3
 
-STS EICRA, R19 ; confg INT0 en flanco ascendente
-SBI EIMSK, INT0 ; habilitar INT0
-CBI EIFR, INTF0 ; borrar banderas de interrupcion
-SEI ; habilitar interrupciones globales
+	OUT DDRD, R16 ; puerto D entrada
+	OUT PORTD, R18 ; activar pullup en PD2 ; y PD3
+	OUT DDRB, R17 ; puerto B salida
+	OUT PORTB, R16 ; valor inicial cero
+
+	STS EICRA, R19 ; confg INT0 en flanco ascendente
+	SBI EIMSK, INT0 ; habilitar INT0
+
+	CBI EIFR, INTF0 ; borrar banderas de interrupcion
+	SEI ; habilitar interrupciones globales
+
+ISR_INT0:
+
+RETARDO:
+	LDI R22, 8
+I1:	
+	LDI R23, 255
+I2:
+	LDI R24, 255
+I3:
+	DEC R24
+	BRNE I3
+	DEC R23
+	BRNE I2
+	DEC R22
+	BRNE I1
+	RET
+	
 
 
 
